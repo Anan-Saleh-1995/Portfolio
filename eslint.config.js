f866e564b3dd55033a19a09304ff386618e3ca89
Anan-Saleh-1995 <anansaleh1995@gmail.com>
2025-10-22 04:41:46 +0300

    Add eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -19,5 +20,24 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      '@stylistic': stylistic
+    },
+    rules: {
+      '@stylistic/semi': ["error", "always"],
+      '@stylistic/eol-last': ["error", "always"],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/comma-dangle': ['error', 'only-multiline'],
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+      '@stylistic/no-multi-spaces': "error",
+      '@stylistic/arrow-spacing': "error",
+      '@stylistic/arrow-parens': ["error", "always"],
+      '@stylistic/no-trailing-spaces': "error",
+      '@stylistic/array-bracket-spacing': ["error", "never"],
+      '@stylistic/block-spacing': "error",
+      '@stylistic/brace-style': "error",
+      '@stylistic/comma-spacing': ["error", { "before": false, "after": true }]
+    }
   },
 ])
