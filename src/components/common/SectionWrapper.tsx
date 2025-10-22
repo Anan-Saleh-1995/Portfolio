4ea3916b625db6d9f06a8d5435fb1b224eccb2b7
Anan-Saleh-1995 <anansaleh1995@gmail.com>
2025-10-22 12:08:15 +0300

    Add collapsible to main sections

--- src/components/common/SectionWrapper.tsx
+++ src/components/common/SectionWrapper.tsx
@@ -1,8 +1,33 @@
+import { useState } from 'react';
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@/components/ui/collapsible';
+import { DrawingPinFilledIcon, DrawingPinIcon } from '@radix-ui/react-icons';
 import type { SectionWrapperProps } from '@/lib/types';
 
-export const SectionWrapper = ({ id, title, children }: SectionWrapperProps) => (
-  <section id={id} className="px-6 py-16 mx-auto">
-    <h2 className="text-2xl font-bold mb-8">{title}</h2>
-    {children}
-  </section>
-);
+export const SectionWrapper = ({ id, title, children }: SectionWrapperProps) => {
+  const [open, setOpen] = useState<boolean>(true);
+  return (
+    <section id={id} className="px-6 py-16 mx-auto">
+      <Collapsible
+        open={open}
+        onOpenChange={setOpen}
+      >
+        <div className="flex gap-2 mb-8">
+          <h2 className="text-2xl font-bold">{title}</h2>
+          <CollapsibleTrigger
+            className={`rounded-full hover:cursor-pointer ${open? 'text-emerald-400' : ''}`}
+          >
+            {open ? <DrawingPinFilledIcon /> : <DrawingPinIcon />}
+          </CollapsibleTrigger>
+        </div>
+        <CollapsibleContent>
+          {children}
+        </CollapsibleContent>
+      </Collapsible>
+    </section>
+  );
+};
+
