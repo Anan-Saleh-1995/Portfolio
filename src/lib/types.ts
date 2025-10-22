de8d949e95b2278df7563500b5e732b35f97a1cd
Anan-Saleh-1995 <anansaleh1995@gmail.com>
2025-10-22 05:04:30 +0300

    Add project types

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,3 +1,44 @@
+import React from 'react';
 import type { THEME } from '@/lib/utils';
 
 export type Theme = (typeof THEME)[keyof typeof THEME];
+
+export type SectionWrapperProps = {
+  id?: string;
+  title: string;
+  children: React.ReactNode;
+};
+
+export type SocialLink = {
+  label: string;
+  href: string;
+  icon: React.ElementType;
+  ariaLabel?: string;
+};
+
+export type ExperienceTypes = {
+  id: number,
+  role: string
+  company: string
+  period: string
+  location: string
+  description: string,
+  tech: string[]
+};
+
+export type Project = {
+  id: number;
+  title: string;
+  description: string;
+  img: string;
+  techStack: string[];
+  github: string;
+  liveDemo: string;
+};
+
+export type ContactForm = {
+  name: string;
+  email: string;
+  message: string;
+  website: string;
+};
